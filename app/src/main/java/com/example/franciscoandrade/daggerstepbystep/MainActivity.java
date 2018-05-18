3064e6eeb50747c441b450e89e096bf56eb7e3ad
7sco <xaviandrade14@gmail.com>
2018-05-18 15:43:57 -0400

    Simple app explaining benefits of dagger, and when it would be used

--- app/src/main/java/com/example/franciscoandrade/daggerstepbystep/MainActivity.java
+++ app/src/main/java/com/example/franciscoandrade/daggerstepbystep/MainActivity.java
@@ -2,12 +2,65 @@ package com.example.franciscoandrade.daggerstepbystep;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText memberId;
+    private Button submitButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        memberId = findViewById(R.id.etMemberId);
+        submitButton = findViewById(R.id.btnSubmit);
+
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((memberId.getText().toString().equals(""))) {
+                    Toast.makeText(getApplicationContext(), "Member ID is empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    /*
+                        -Construct a new member Data Object
+                        -Call  checkMemberStatus and pass in the user input
+                        -We save String and pass in as a Toast message if the user has been granted access or denied,
+                            based on list of members from MemberDataManager.class
+                     */
+                    String input = memberId.getText().toString();
+                    String result = new MemberDataManager().checkMemberStatus(input);
+                    Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+
+                    /*
+                        By using this approach we go against SOLID principle,()
+                     */
+
+                    /**
+                     * Objects should not create new objects of other types by them selves
+                     * They should get other objects via abstractions (using Interfaces)
+                     * In real life we do not make everything by ourselves, we go to a supermarket and buy them
+                     */
+
+                    /*
+                      Every time we pass in a text we create a new MemberDataManager object,
+                      for task like connect with rest API, convert JSON data, and connect with local DB
+                      Inefficient, inecesary use of resources
+                     */
+
+                    /**
+                     * By using Dagger we can create a mechanism to create a one object of the user data manager at the begining,
+                     * and inject that one object again and again to the main activity, whenever required
+                     */
+
+                }
+            }
+        });
     }
 }
